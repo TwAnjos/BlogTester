d63b6a824791fe879122f146ce6c6fd849cd4db4
Thiago Anjos <thiagowaltrick@gmail.com>
2021-04-05 18:02:56 -0300

    up project

---
+++ src/main/java/com/spring/BlogTester/BlogTesterApplication.java
@@ -0,0 +1,13 @@
+package com.spring.BlogTester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/BlogTester/BlogTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.BlogTester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
