5ecca65432a083f60deab4101eecc3c9057a30ec
Thiago Anjos <thiagowaltrick@gmail.com>
2021-04-08 18:26:24 -0300

    add new controller getPostById

--- src/main/java/com/spring/BlogTester/controller/PostController.java
+++ src/main/java/com/spring/BlogTester/controller/PostController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,12 +23,23 @@ public class PostController {
 	@RequestMapping(value = "/posts", method = RequestMethod.GET)
 	public ModelAndView getPosts() {
 		//ModelAndView mv = new ModelAndView( viewName: "posts");
-		ModelAndView mv = new ModelAndView("posts");
+		ModelAndView mv = new ModelAndView("posts"); //posts.html
 		List<Post> posts = iPostService.GetAll();
 		//mv.addObject(attributeName: "posts", posts);
 		mv.addObject("mvListPosts", posts);
 		return mv;
 	}
+	
+	
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostById(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("postDetails"); //postDetails.html
+		Post postId = iPostService.GetById(id);
+		mv.addObject("mvPostId", postId);
+		return mv;
+	}
+	
+	
 	
 	
 	
